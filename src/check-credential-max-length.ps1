c53b9c5db1b01ddb5da7e9c6898217fae568112b
Yutaka Omido <yutaka.omido@gmail.com>
2021-12-03 20:17:03 +0900

    remove item if it was still remained

--- src/check-credential-max-length.ps1
+++ src/check-credential-max-length.ps1
@@ -1,5 +1,6 @@
 $TargetName = "MidoApp_MaxLength_Test"
 $MaxLength = 1000
+$ItemRemaining = $False
 
 . ./lib.ps1
 
@@ -14,14 +15,18 @@ function CreatePasswordWithLength($Length) {
 try {
   for($i=1; i -le $MaxLength; $i++) {
     echo "Creating Generic Credential with Password Length ${i} to Windows Credential Manager"
+    $ItemRemaining = $True
     $Password = CreatePasswordWithLength $i
     SaveGenericSessionCredential $TargetName $Password
     echo "Removing Generic Credential with Password Length ${i} from Windows Credential Manager"
     RemoveGenericCredential $TargetName
+    $ItemRemaining = $False
   }      
 } catch {
   echo $error
 } finally {
-
+  if ($ItemRemaining) {
+    RemoveGenericCredential $TargetName
+  }
 }
 
